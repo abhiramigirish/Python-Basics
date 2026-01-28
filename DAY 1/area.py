891967e8ad9030c21783c83390d74ed399d27856
abhiramigirish <abhiramigirishn@gmail.com>
2026-01-28 12:45:26 +0530

    Add Day 1 Python practice

---
+++ DAY 1/area.py
@@ -0,0 +1,6 @@
+H = 5
+W = 2
+Area = H * W
+Perimeter = 2 * (H + W)
+print("Area=", Area)
+print("perimeter=", Perimeter)
---
+++ DAY 1/var.py
@@ -0,0 +1,6 @@
+name="Neya"
+Age=13
+height=155.5
+print(type(name))
+print(type(Age))
+print(type(height))
